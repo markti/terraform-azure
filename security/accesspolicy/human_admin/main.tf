85d086e2c3e75b0531a1c982eacbc03f9b40b67b
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-14 20:58:09 -0400

    updated user data ref

--- security/accesspolicy/human_admin/main.tf
+++ security/accesspolicy/human_admin/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_key_vault_access_policy" "policy" {
   key_vault_id = var.keyvault_id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = azuread_user.user_account.object_id
+  object_id = data.azuread_user.user_account.object_id
 
   key_permissions = [
     "get", "list", "create", "update", "decrypt"
