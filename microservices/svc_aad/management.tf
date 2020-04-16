0fb8d742286fce8eeed5cfb3d65b2c5e54099ac0
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-15 22:58:49 -0400

    pass client id on

--- microservices/svc_aad/management.tf
+++ microservices/svc_aad/management.tf
@@ -11,6 +11,6 @@ module "fn_apim_api" {
   path                  = var.api_path
   function_name         = module.api_fn.name
   function_key          = module.api_fn.function_key
-  client_id             = 
+  client_id             = var.client_id
 
 }
