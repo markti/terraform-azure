6c81d2521ecf0dbccc9eac8971add7104fed1415
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-22 11:34:38 -0400

    remove dupe env var

--- microservices/hub/variables.tf
+++ microservices/hub/variables.tf
@@ -2,7 +2,6 @@ variable "resource_group_name" { }
 variable "location" { }
 variable "app_name" { }
 variable "env_name" { }
-variable "env_name" { }
 variable "location_suffix" { }
 variable "terraform_application_id" {
     description = "This is needed to allow Terraform the ability to provision secrets within the key vaults."
