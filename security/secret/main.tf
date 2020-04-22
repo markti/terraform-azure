9c122d06c97b7ac5ee60b62b3dca55dd6c1ec25b
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-22 02:11:30 -0400

    keyvault secret

---
+++ security/secret/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.name
+  value        = var.value
+  key_vault_id = var.keyvault_id
+
+  tags = {
+    environment = var.env_name
+  }
+}
---
+++ security/secret/variables.tf
@@ -0,0 +1,4 @@
+variable "keyvault_id" { }
+variable "env_name" { }
+variable "name" { }
+variable "value" { }
