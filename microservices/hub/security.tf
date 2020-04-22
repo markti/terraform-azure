63c10698340989fa232cb6857f790014828eec20
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-22 08:25:30 -0400

    move front door out of backend hub

--- microservices/hub/security.tf
+++ microservices/hub/security.tf
@@ -22,20 +22,3 @@ module "terraform_admin" {
   application_id        = var.terraform_application_id
 
 }
-
-module "frontdoor" {
-  
-  source                = "../../net/frontdoor"
-
-  app_name              = var.app_name
-  env_name              = var.env_name
-  
-  name                  = "${var.app_name}-${var.env_name}"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-
-  frontend_hostname     = var.frontend_hostname
-  backend_fqdn          = "${module.api_management.name}.azure-api.net"
-  backend_host_header   = var.frontend_hostname
-
-}
