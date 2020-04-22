1bc7d1417a8f99863f4bc650b50c0308817fcecb
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-22 02:27:56 -0400

    allow network access to keyvault

--- security/keyvault/main.tf
+++ security/keyvault/main.tf
@@ -12,7 +12,10 @@ resource "azurerm_key_vault" "keyvault" {
   sku_name = "standard"
 
   network_acls {
+    /*
     default_action = "Deny"
+    */
+    default_action = "Allow"
     bypass         = "AzureServices"
   }
 
