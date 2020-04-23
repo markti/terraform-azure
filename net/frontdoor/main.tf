73be257c103e9e5df37ac3db110d4021458939a3
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-23 13:28:57 -0400

    add custom_https_configuration

--- net/frontdoor/main.tf
+++ net/frontdoor/main.tf
@@ -42,5 +42,10 @@ resource "azurerm_frontdoor" "frontdoor" {
     name                              = "PrimaryEndpoint"
     host_name                         = var.frontend_hostname
     custom_https_provisioning_enabled = true
+    
+    custom_https_configuration {
+      certificate_source    = "FrontDoor"
+      minimum_tls_version   = "1.2"
+    }
   }
 }
