fb039c9c695e1d3a1e231cface484cf7ff6fd1a0
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-23 09:10:01 -0400

    svc output sp details

--- microservices/svc_baseline/outputs.tf
+++ microservices/svc_baseline/outputs.tf
@@ -9,3 +9,9 @@ output "function_name" {
 output "function_key" {
     value = module.api_fn.function_key
 }
+output "client_id" {
+    value = module.service_principal.client_id
+}
+output "client_secret" {
+    value = module.service_principal.client_secret
+}
