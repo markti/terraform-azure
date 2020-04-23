ae14d9f75b1731451505da3655549e89e040a4f6
Mark Tinderholt <mark@vitalsigyn.com>
2020-04-23 09:12:36 -0400

    removed interpolation

--- api/management/hub/auth/b2c/main.tf
+++ api/management/hub/auth/b2c/main.tf
@@ -7,8 +7,8 @@ resource "azurerm_api_management_authorization_server" "apim_auth" {
   resource_group_name          = var.resource_group_name
   display_name                 = var.name
   authorization_endpoint       = "https://${var.tenant_name}.b2clogin.com/${var.tenant_name}.onmicrosoft.com/oauth2/v2.0/authorize?p=${var.signin_policy}"
-  client_id                    = "${var.client_id}"
-  client_secret                = "${var.client_secret}"
+  client_id                    = var.client_id
+  client_secret                = var.client_secret
   client_registration_endpoint = "https://${var.tenant_name}.b2clogin.com/${var.tenant_name}.onmicrosoft.com/oauth2/v2.0/authorize?p=${var.signin_policy}"
   token_endpoint               = "https://${var.tenant_name}.b2clogin.com/${var.tenant_name}.onmicrosoft.com/oauth2/v2.0/token?p=${var.signin_policy}"
 
