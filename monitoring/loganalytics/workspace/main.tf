ccf6c9f28b7be17b0c0957e48109ff521f165afb
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-10 19:14:13 -0400

    log analytics tags

--- monitoring/loganalytics/workspace/main.tf
+++ monitoring/loganalytics/workspace/main.tf
@@ -4,6 +4,11 @@ resource "azurerm_log_analytics_workspace" "workspace" {
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+
+  tags = {
+    app = var.app_name
+    env = var.env_name
+  }
 }
 
 
